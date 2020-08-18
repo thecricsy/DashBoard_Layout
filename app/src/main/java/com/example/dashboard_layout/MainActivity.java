3bfc83874412d6250f076a1161d2386a08ee66ae
phanthanh.it.hutech@gmail.com <phanthanh.it.hutech@gmail.com>
2020-08-18 15:55:40 +0700

    Update_Layout

--- app/src/main/java/com/example/dashboard_layout/MainActivity.java
+++ app/src/main/java/com/example/dashboard_layout/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.dashboard_layout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Switch;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent = new Intent(MainActivity.this, Login_Layout.class);
+                switch (item.getItemId())
+                {
+                    case R.id.menuSave:
+                        startActivity(intent);
+                        break;
+                    case R.id.menuAdd:
+                        startActivity(intent);
+                        break;
+                    case R.id.menuAccount:
+                        startActivity(intent);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
