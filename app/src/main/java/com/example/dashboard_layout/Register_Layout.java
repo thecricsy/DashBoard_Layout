023993458a164aca803e56548ebb28c5fadee8ff
phanthanh.it.hutech@gmail.com <phanthanh.it.hutech@gmail.com>
2020-08-19 21:54:04 +0700

    Final_layout

--- app/src/main/java/com/example/dashboard_layout/Register_Layout.java
+++ app/src/main/java/com/example/dashboard_layout/Register_Layout.java
@@ -5,37 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
-public class Login_Layout extends AppCompatActivity {
+import org.w3c.dom.Text;
 
-    Button btnCreate;
+public class Register_Layout extends AppCompatActivity {
+
+    TextView txtAlreadyLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login__layout);
+        setContentView(R.layout.activity_register__layout);
 
         linkViews();
         addEvents();
     }
 
     private void addEvents() {
-        btnCreate.setOnClickListener(new View.OnClickListener() {
+        txtAlreadyLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            openCreateLayout();
+                openLoginLayout();
             }
         });
     }
 
-    private void openCreateLayout() {
-        Intent intent = new Intent(Login_Layout.this, Register_Layout.class);
+    private void openLoginLayout() {
+        Intent intent = new Intent(Register_Layout.this, Login_Layout.class);
         startActivity(intent);
     }
 
     private void linkViews() {
-        btnCreate = findViewById(R.id.btnCreate);
+        txtAlreadyLogin = findViewById(R.id.txtAlreadyLogin);
     }
-
 }
